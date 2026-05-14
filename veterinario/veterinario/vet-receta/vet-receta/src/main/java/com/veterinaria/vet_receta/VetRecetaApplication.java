6a5d306c9fa02054778fbd6dcf65a2a8ca8db825
moboomtoon <franci.rojash@duocuc.cl>
2026-05-14 18:13:49 -0400

    vet-receta

---
+++ veterinario/veterinario/vet-receta/vet-receta/src/main/java/com/veterinaria/vet_receta/VetRecetaApplication.java
@@ -0,0 +1,13 @@
+package com.veterinaria.vet_receta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetRecetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetRecetaApplication.class, args);
+	}
+
+}
